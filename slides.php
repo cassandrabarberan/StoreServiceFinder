aafa984bd35a2e0dab71caecc671146142d97c2b
Carvie Adam <96063536+CarvzAdam@users.noreply.github.com>
2022-10-25 13:33:39 +0800

    carvz 10-25-22

--- slides.php
+++ slides.php
@@ -1,84 +1,24 @@
- <div id="main-slider" class="flexslider">
+<div id="main-slider" class="flexslider">
             <ul class="slides">
-      <!--         <li>
-                <img src="<?php echo web_root; ?>plugins/home-plugins/img/slides/Small-Rime.jpg" alt="" />
+              <li>
+                <img src="./plugins/home-plugins/img/slides/3.png" alt="" />
                 <div class="flex-caption">
-                    <h3>innovation</h3> 
-          <p>We create the opportunities</p> 
-           
+                 <!--    <h3>The Generics Pharmacy</h3> 
+                       <p>TG Pagpagaling nang sakit</p>  -->
                 </div>
               </li>
               <li>
-                <img src="<?php echo web_root; ?>plugins/home-plugins/img/slides/maxresdefault.jpg" alt="" />
-                <div class="flex-caption">
-                    <h3>Pharmacy Finder</h3> 
-          <p>Success depends on work</p> 
-           
-                </div>
-              </li> -->
-                  <li>
-                <img src="<?php echo web_root; ?>plugins/home-plugins/img/slides/banner.png" alt="" />
+                <img src="./plugins/home-plugins/img/slides/3.png" alt="" />
                 <div class="flex-caption">
                  <!--    <h3>The Generics Pharmacy</h3> 
-                       <p>TG Pagpagaling nang sakit</p>  -->
-           
-                </div>
-              </li>
-                  <li>
-                <img src="<?php echo web_root; ?>plugins/home-plugins/img/slides/banner.png" alt="" />
-                <div class="flex-caption">
-                 <!--    <h3>The Generics Pharmacy</h3> 
-                       <p>TG Pagpagaling nang sakit</p>  -->
-           
+                       <p>TG Pagpagaling nang sakit</p>  --> 
                 </div>
               </li>
               <li>
-                <img src="<?php echo web_root; ?>plugins/home-plugins/img/slides/banner.png" alt="" />
+                <img src="./plugins/home-plugins/img/slides/3.png" alt="" />
                 <div class="flex-caption">
                  <!--    <h3>The Generics Pharmacy</h3> 
                        <p>TG Pagpagaling nang sakit</p>  -->
-           
-                </div>
-              </li>
-              <li>
-                <img src="<?php echo web_root; ?>plugins/home-plugins/img/slides/banner.png" alt="" />
-                <div class="flex-caption">
-                 <!--    <h3>The Generics Pharmacy</h3> 
-                       <p>TG Pagpagaling nang sakit</p>  -->
-           
-                </div>
-              </li>
-
-              <li>
-                <img src="<?php echo web_root; ?>plugins/home-plugins/img/slides/banner.png" alt="" />
-                <div class="flex-caption">
-                 <!--    <h3>The Generics Pharmacy</h3> 
-                       <p>TG Pagpagaling nang sakit</p>  -->
-           
-                </div>
-              </li>
-              <li>
-                <img src="<?php echo web_root; ?>plugins/home-plugins/img/slides/banner.png" alt="" />
-                <div class="flex-caption">
-                 <!--    <h3>The Generics Pharmacy</h3> 
-                       <p>TG Pagpagaling nang sakit</p>  -->
-           
-                </div>
-              </li>
-              <li>
-                <img src="<?php echo web_root; ?>plugins/home-plugins/img/slides/banner.png" alt="" />
-                <div class="flex-caption">
-                 <!--    <h3>The Generics Pharmacy</h3> 
-                       <p>TG Pagpagaling nang sakit</p>  -->
-           
-                </div>
-              </li>
-              <li>
-                <img src="<?php echo web_root; ?>plugins/home-plugins/img/slides/banner.png" alt="" />
-                <div class="flex-caption">
-                 <!--    <h3>The Generics Pharmacy</h3> 
-                       <p>TG Pagpagaling nang sakit</p>  -->
-           
                 </div>
               </li>
             </ul>
